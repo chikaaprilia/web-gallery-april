1f8485c823093041ca46612908a1c7221bc074f8
chika <chikaaprilia245@gmail.com>
2024-02-26 17:14:30 +0700

    first commit

---
+++ resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.layout')
+@section('content')
+    <div class="card bg-primary text-light">
+        <div class="card-body">
+            <h1>Selamat datang di halaman dashboard galery smk alhafidz</h1>
+        </div>
+    </div>
+@endsection
